81a7588b39abd9bbfdf27a514794faa78cbddaad
Gilles Callebaut <callebaut.gilles@gmail.com>
2020-09-25 14:59:33 +0200

    Update find-my-device.py

--- find-my-device/find-my-device.py
+++ find-my-device/find-my-device.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import socket
 import RPi.GPIO as GPIO
+import select
 
 import time
 
+
 try:
 	pin_num = 37
+	state = False
+	blink = False
+
 	hostname = socket.gethostname()
 
 	GPIO.setmode(GPIO.BOARD)
@@ -13,9 +18,9 @@ try:
 	GPIO.setup(pin_num, GPIO.OUT)
 
 	print(f"Starting listening to port 37020 at {hostname}")
-	
 
 	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+	
 
 	client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
@@ -23,14 +28,25 @@ try:
 	client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 	client.bind(("", 37020))
+
+	client.setblocking(0)
+
+	
+
 	while True:
-	    data, addr = client.recvfrom(1024)
-	    print(str(data))
-	    if hostname in str(data):
-	    	state = True
-	    	for i in range(20):
-	    		GPIO.output(pin_num, state)
-	    		state = not state
-	    		time.sleep(0.2)
+		if blink:
+			GPIO.output(pin_num, state)
+			state = not state
+			time.sleep(0.2)
+
+		read_sockets, write_sockets, error_sockets = select.select([client], [], [], 0)
+
+		for sock in read_sockets:
+			#incoming message from remote server
+			if sock == client:
+				data, addr = client.recvfrom(1024)
+				print(str(data))
+				if hostname in str(data):
+					blink = not blink
 except KeyboardInterrupt:
-    GPIO.cleanup()
+	GPIO.cleanup()
